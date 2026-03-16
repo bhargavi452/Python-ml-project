7df1456e340bf3d8e2d8290de43ad4932c2a5981
bhargavi452 <bhargavibandi864@gmail.com>
2026-03-16 13:39:53 +0530

    Add files via upload
    "Add initial project files"

---
+++ project(2).py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+df=pd.read_csv('heart.csv')
+df.head()
+print(df)
+#EDA
+print(df.columns)
+print(df.shape)
+print(df.info())
+print(df.describe)
+print(df.duplicated().sum())
+print(df['HeartDisease'].value_counts())
+print(df.isnull().sum())
+def plotting(var,num):
+    plt.subplot(2,2,num)
+    sns.histplot(df[var],kde=True)
+plotting('Age',1)   
+plotting('RestingBP',2)   
+plotting('Cholesterol',3)   
+plotting('MaxHR',4)
+plt.tight_layout()
+plt.show()
+print(df['Cholesterol'].value_counts())
+ch_mean=df.loc[df['Cholesterol']!=0,'Cholesterol'].mean()
+df['Cholesterol']=df['Cholesterol'].replace(0,ch_mean)
+print(df['Cholesterol'].head())
+df['Cholesterol']=df['Cholesterol'].round(2)
+resting_bp_mean=df.loc[df['RestingBP']!=0,'RestingBP'].mean()
+df['RestingBP']=df['RestingBP'].replace(0,resting_bp_mean)
+print(df['RestingBP'].head())
+df['RestingBP']=df['RestingBP'].round(2)
+def plotting(var,num):
+    plt.subplot(2,2,num)
+    sns.histplot(df[var],kde=True)
+plotting('Age',1)   
+plotting('RestingBP',2)   
+plotting('Cholesterol',3)   
+plotting('MaxHR',4)
+plt.tight_layout()
+plt.show()
+import sheryanalysis as sh
+sh.analyze(df)
+sns.countplot(x=df['Sex'],hue=df['HeartDisease'])
+plt.show()
+sns.countplot(x=df['ChestPainType'],hue=df['HeartDisease'])
+plt.show()
+sns.countplot(x=df['FastingBS'],hue=df['HeartDisease'])
+plt.show()
+sns.boxplot(x='HeartDisease',y='Cholesterol',data=df)
+plt.show()
+sns.violinplot(x='HeartDisease',y='Age',data=df)
+plt.show()
+sns.heatmap(df.corr(numeric_only=True),annot=True)
+plt.show()
+#data  preprocessing and cleaning
+df_encode=pd.get_dummies(df,drop_first=True)
+print(df_encode)
+df_encode=df_encode.astype(int)
+print(df_encode)
+from sklearn.preprocessing import StandardScaler
+numerical_cols=['Age','RestingBP','Cholesterol','MaxHR','Oldpeak']
+scalar=StandardScaler()
+df_encode[numerical_cols]=scalar.fit_transform(df_encode[numerical_cols])
+df_encode.head()
